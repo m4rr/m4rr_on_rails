348a64cc8896e6b85eea599095a1490a1524a9d1
Marat S <remarr@gmail.com>
2015-05-13 01:09:56 +0300

    new central dots

--- vendor/assets/javascripts/map.js
+++ vendor/assets/javascripts/map.js
@@ -7,7 +7,9 @@ function buildMap(markers){
                 map: {
                     options: {
                         zoom: 4,
-                        center: new google.maps.LatLng(52.31, 13.22),
+                        center: new google.maps.LatLng(52.31, 13.22), // berlin
+                      //center: new google.maps.LatLng(43.541050, -40.602783), // atl ocean
+                      //center: new google.maps.LatLng(51.51, 0), // london
                         styles: mapStyleMyFork,
                         disableDefaultUI: true,
                         mapTypeControl: false,
