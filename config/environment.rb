6bc70c9369ec8785ba39e055915e46be2d578a70
Marat S <remarr@gmail.com>
2015-10-16 01:59:31 +0300

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,13 @@
+# force Rails into production mode when                          
+# you don't control web/app server and can't set it the proper way                  
+# ENV['RAILS_ENV'] ||= 'production'
+
+#config.force_ssl = true
+#config.middleware.use Rack::SslEnforcer,
+#  :redirect_to => 'https://example.com',    # For when behind a proxy, like nginx
+#  :only => [/^\/admin\//, /^\/authors\//],  # Force SSL on everything behind /admin and /authors
+#  :strict => true                           # Force no-SSL for everything else
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
