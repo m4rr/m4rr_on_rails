f5a22579d849bc06f79c28709c7e1093289f50d8
Marat S <remarr@gmail.com>
2015-10-16 04:04:20 +0300

    mauth

--- app/controllers/mauth_controller.rb
+++ app/controllers/mauth_controller.rb
@@ -1,12 +1,82 @@
 class MauthController < ApplicationController
   layout 'mauth'
+  before_action :set_betum, only: [:show, :edit, :update, :destroy]
 
+  # GET /beta
+  # GET /beta.json
   def index
+    new
+    # @beta = Betum.all
+    # redirect_to new
+  end
+
+  # GET /beta/1
+  # GET /beta/1.json
+  def show
+  end
+
+  # GET /beta/new
+  def new
     @betum = Betum.new
   end
 
-  def create
-    
+  # GET /beta/1/edit
+  def edit
   end
+
+  # POST /beta
+  # POST /beta.json
+  def create
+    @betum = Betum.new(betum_params)
+
+    respond_to do |format|
+      if @betum.save
+        # format.html { redirect_to @betum, notice: 'Betum was successfully created.' }
+        format.json { render :show, status: :created, location: @betum }
+      else
+        # format.html { render :new }
+        format.json { render json: @betum.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /beta/1
+  # PATCH/PUT /beta/1.json
+  def update
+    respond_to do |format|
+      if @betum.update(betum_params)
+        format.html { redirect_to @betum, notice: 'Betum was successfully updated.' }
+        format.json { render :show, status: :ok, location: @betum }
+      else
+        format.html { render :edit }
+        format.json { render json: @betum.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /beta/1
+  # DELETE /beta/1.json
+  def destroy
+    @betum.destroy
+    respond_to do |format|
+      format.html { redirect_to beta_url, notice: 'Betum was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_betum
+      @betum = Betum.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def betum_params
+      params.require(:betum).permit(:first_name, :last_name, :email, :desc)
+    end
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get  'mauth' => 'mauth#index'
-  post 'mauth' => 'mauth#create'
+  # get  'mauth' => 'mauth#index'
+  # post 'mauth' => 'mauth#create'
+
+  resources :betum, controller: :mauth, path: 'mauth', only: [:index, :create] # , as: 'mauth'
 
   get '@sync' => 'welcome#sync'
 
