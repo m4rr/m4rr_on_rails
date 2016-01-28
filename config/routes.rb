132e39c67125c78717b7b2d28a5d98d63c4fafc5
Marat S <remarr@gmail.com>
2016-01-29 00:22:56 +0300

    cv routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "welcome#index"
   get '/marat-saytakov-resume.pdf', to: redirect('/marat-saytakov-cv.pdf')
+  get '/cv.pdf', to: redirect('/marat-saytakov-cv.pdf')
+  get '/cv', to: redirect('/marat-saytakov-cv.pdf')
   get "@sync", to: "welcome#sync"
   get "mauth/sanmarco", to: "mauth#sanmarco"
   resources :betum, controller: :mauth, path: 'mauth', only: [:index, :create, :show] # , as: "mauth"
