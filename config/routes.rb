ca0c8f77fce11a6b83b6c82626c2fe39009a4707
Marat S <remarr@gmail.com>
2016-03-17 15:28:34 +0300

    un-route /relocated
    because https://europass.cedefop.europa.eu/en/documents/curriculum-vitae

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-
   root "welcome#index"
+
   get '/marat-saytakov-resume.pdf', to: redirect('/marat-saytakov-cv.pdf')
   get '/cv.pdf', to: redirect('/marat-saytakov-cv.pdf')
   get '/cv', to: redirect('/marat-saytakov-cv.pdf')
   get "@sync", to: "welcome#sync"
 
+  resources :betum, controller: :mauth, path: 'mauth', only: [:index, :create, :show] # , as: "mauth"
   get "mauth/sanmarco", to: "mauth#sanmarco"
   get "mauth/privacy-policy", to: "mauth#privacy_policy"
-  resources :betum, controller: :mauth, path: 'mauth', only: [:index, :create, :show] # , as: "mauth"
-
-  get '/relocated', to: 'relocated#hello'
 end
