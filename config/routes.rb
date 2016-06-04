0e5d16e2685d41c4a1a352ffed73d3952619087b
Marat Saytakov <remarr@gmail.com>
2016-06-04 16:13:24 +0300

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  root "welcome#index"
+  root 'welcome#index'
 
-  get '/marat-saytakov-resume.pdf', to: redirect('/marat-saytakov-cv.pdf')
-  get '/cv.pdf', to: redirect('/marat-saytakov-cv.pdf')
-  get '/cv', to: redirect('/marat-saytakov-cv.pdf')
-  get "@sync", to: "welcome#sync"
+  # Error 451
+  get '/451', to: redirect('/404', status: 451)
 
+  # CV
+  @cv_pdf_url ||= '/marat-saytakov-cv.pdf'
+  get '/marat-saytakov-resume.pdf', to: redirect(@cv_pdf_url)
+  get '/cv.pdf', to: redirect(@cv_pdf_url)
+  get '/cv', to: redirect(@cv_pdf_url)
+
+  # Auth-based sync map data with Tripster
+  get '@sync', to: 'welcome#sync'
+
+  # Mauth
   get 'mauth', to: 'mauth#index'
-  get "mauth/privacy-policy", to: "mauth#privacy_policy"
+  get 'mauth/privacy-policy', to: 'mauth#privacy_policy'
 end
